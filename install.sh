54e5bb16b0d4df94affb23ac25cf347c34adcc69
Sad Paradise <sadparadise@gmail.com>
2023-05-21 20:40:48 -0300

    obs

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 systemctl enable --now systemd-networkd NetworkManager
 
-pacman -S sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop
+pacman -S sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio
 
 useradd -g wheel gui
