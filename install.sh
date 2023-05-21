a58bfacde463f4fc21c9c67e7a1cb04cff8f649d
Sad Paradise <sadparadise@gmail.com>
2023-05-21 20:33:02 -0300

    mudei a ordem

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 systemctl enable --now systemd-networkd NetworkManager
 
-useradd -g wheel gui
-
 pacman -S sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji
+
+useradd -g wheel gui
