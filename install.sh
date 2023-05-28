0f0291c553150875bef21e2f5413fd2d7c764e9e
Sad Paradise <sadparadise@gmail.com>
2023-05-28 12:05:48 -0300

    aos poucos toma forma

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 systemctl enable --now systemd-networkd NetworkManager
 
-pacman -S sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio alsa-utils
+cp mirrorlist /etc/pacman.d/mirrorlist
+
+pacman -Syu sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio alsa-utils
+
+cp doas.conf /etc/doas.conf
 
 useradd -g wheel gui
+usermod -aG audio gui
+usermod -aG sys gui
