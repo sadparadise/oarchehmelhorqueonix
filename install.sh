baeac1afd74b7957e3a91785d27b8c6c7bf72eb4
Sad Paradise <sadparadise@gmail.com>
2023-05-30 17:26:50 -0300

    preparatórios para uma futura migração pro wayland

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@ systemctl enable --now systemd-networkd NetworkManager
 
 cp mirrorlist /etc/pacman.d/mirrorlist
 
-pacman -Syu sudo opendoas xorg xorg-xinit xbindkeys syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio alsa-utils tor torbrowser-launcher libreoffice-still
+pacman -Syu sudo opendoas xorg xorg-xinit xbindkeys xf86-video-intel vulkan-intel mesa syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio alsa-utils tor torbrowser-launcher libreoffice-still polkit
+
+# For a future migration to wayland:
+# pacman -Syu sway swaybg wmenu xdg-desktop-portal-wlr
 
 cp doas.conf /etc/doas.conf
 
