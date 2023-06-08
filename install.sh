079895b1261d8cdc669ce64e9148511cdcf62573
Sad Paradise <sadparadise@gmail.com>
2023-06-08 00:32:03 -0300

    migrei pro wayland :P

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@ systemctl enable --now systemd-networkd NetworkManager
 
 cp mirrorlist /etc/pacman.d/mirrorlist
 
-pacman -Syu sudo opendoas xorg xorg-xinit xbindkeys xf86-video-intel vulkan-intel mesa syncthing xlockmore maim pulseaudio pulseaudio-alsa alsa-firmware sof-firmware i3-wm i3status sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst hsetroot xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano xclip man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio alsa-utils tor torbrowser-launcher libreoffice-still polkit
+# Generics
+pacman -Syu sudo opendoas i3status xf86-video-intel vulkan-intel mesa syncthing pulseaudio pulseaudio-alsa alsa-firmware alsa-utils sof-firmware sxiv mpv zathura zathura-pdf-poppler stow gimp thunderbird qbittorrent udiskie zsh zsh-completions dunst xdg-user-dirs firefox pcmanfm kitty emacs discord base-devel git lxappearance pavucontrol krecorder pass neovim nano man-pages man-db gnome-themes-extra wqy-zenhei noto-fonts-emoji neofetch htop obs-studio tor torbrowser-launcher libreoffice-still polkit tmux
 
-# For a future migration to wayland:
-# pacman -Syu sway swaybg wmenu xdg-desktop-portal-wlr
+# For xorg
+# pacman -S xorg xorg-xinit xbindkeys xlockmore maim i3-wm hsetroot xclip 
+
+# For wayland
+pacman -S sway swaybg wmenu xdg-desktop-portal-wlr wl-clipboard grim slurp
 
 cp doas.conf /etc/doas.conf
 
